3c7fe446d579ac82ed0b8a3b8858116fa6a6a351
Louis Huy Nguyen <huynguyennbk@gmail.com>
2026-03-20 09:36:09 +0700

    fix:  deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://louishuyng.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
